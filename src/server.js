af4be141042dcd77c244d4415eb7852101906008
Nick Jackson <njnickjackson@yahoo.com>
2022-10-25 20:45:47 -0400

    rename json-server for heroku

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-const jsonServer = require("mock-server");
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("./products.json");
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults({
   static: "./build",
 });
